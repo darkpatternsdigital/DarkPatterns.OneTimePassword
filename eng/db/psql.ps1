639441a3b862dfa89ca55e5b127a38c1297dca13
Matt DeKrey <mattdekrey@gmail.com>
2024-07-23 21:48:20 -0500

    Correct PSQL script to default to docker

--- eng/db/psql.ps1
+++ eng/db/psql.ps1
@@ -1,6 +1,12 @@
 #!/usr/bin/env pwsh
 
+param(
+	[Parameter()][Alias('u')][String] $user = 'postgres',
+	[Parameter()][Alias('d')][String] $database = 'otp',
+	[Parameter(ValueFromRemainingArguments=$true)][String[]]$Args
+)
+
 # Launches the "psql" command in the terminal with a connection to the
 # docker-compose postgres db instance
 
-docker exec -ti db-db-1 psql -U postgres # -d otp
+docker exec -ti db-db-1 psql -U $user -d $database @Args
